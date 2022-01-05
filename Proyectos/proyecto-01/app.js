b011da9d2b4f957ff840c67d215f3a807d710778
Diego Chiapa <dchiapa@est.frba.utn.edu.ar>
2022-01-04 22:53:30 -0300

    update proyecto-01 - Section 3 - Vistas

--- Proyectos/proyecto-01/app.js
+++ Proyectos/proyecto-01/app.js
@@ -2,18 +2,26 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// MOTOR DE VISTAS
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+
 // middleware
 // permite responder con archivos estaticos
 // segun la ruta
 // no es necesario conocer la ruta absoluta
 app.use(express.static(__dirname + "/public"));
 
-app.use((req, res, next) => {
-  res.sendFile(__dirname + "/public/404.html");
+app.get("/", (req, res) => {
+  res.render("index", { titulo: "Index" });
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello World v2 - express");
+app.get("/servicios", (req, res) => {
+  res.render("servicios", { titulo: "Página de Servicios" });
+});
+
+app.use((req, res, next) => {
+  res.render("404", { titulo: "Página no encontrada" });
 });
 
 app.listen(port, () => {
